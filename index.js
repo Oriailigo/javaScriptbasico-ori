09a1649ec7d8e35f5789f75e401b6af433891c50
infosergio2020 <infosergio2020@gmail.com>
2021-08-09 16:16:13 -0300

    sergio copio comentarios de oriana

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // estas son cosas de sergio
+// crear una rama:
+
+// git branch --> te muestra la rama donde estas
+// git branch oriana--> te crea una nueva ramra
+
+// git checkout oriana --> enctro a la rama oriana
